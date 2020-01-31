0836f11fae3f1ad6a754026c5a7b2b6c39e2e5a5
Michel07220823 <michel31703@gmail.com>
2020-01-31 18:00:51 +0800

    Add practice .cpp files.

---
+++ Literal_constant/Literal_constant-1.cpp
@@ -0,0 +1,9 @@
+#include <iostream> 
+using namespace std; 
+
+int main() { 
+    cout << "sizeof(1):\t"   << sizeof("aaa")   << "\n";
+    cout << "sizeof(1.0):\t" << sizeof(1.0) << "\n";
+
+    return 0; 
+}
